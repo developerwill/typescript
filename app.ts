c9f8555c6f8da8ee18addb1118374734e01c859c
developerwill <info@wilsongomes.me>
2022-10-31 18:17:27 +0100

    Working with Tuples (TS type)

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Wilson',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
 
-console.log(`${person.name}`);
+person.role.push('admin');
+
+console.log(`${person.role}`);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toLowerCase());
