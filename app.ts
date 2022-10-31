00f3d73f9b797f8a441f0b10ce9bf96fc5ff24c9
developerwill <info@wilsongomes.me>
2022-10-31 18:01:57 +0100

    Object types

--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
+const person = {
+    name: 'Wilson',
+    age: 30,
+};
 
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
-
-const num1 = 5;
-const num2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(num1, num2, printResult, resultPhrase);
+console.log(`${person.age}`);
