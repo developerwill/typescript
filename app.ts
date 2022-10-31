4bda1bc1c6606a26c016ab1363df1f0874e3fca5
developerwill <info@wilsongomes.me>
2022-10-31 18:43:59 +0100

    Literal types

--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-string') {
     let result: string | number;
 
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Wilson ', 'Gomes');
+const combinedStrinAges = combine('30', '26', 'as-number');
+console.log(combinedStrinAges);
+
+const combineNames = combine('Wilson ', 'Gomes', 'as-string');
 console.log(combineNames);
