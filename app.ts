0a16f5735f8d9eb3eec36b8f9b14b0f739b5c764
developerwill <info@wilsongomes.me>
2022-10-31 18:09:45 +0100

    Arrays types

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 const person = {
     name: 'Wilson',
     age: 30,
+    hobbies: ['Sports', 'Cooking'],
 };
 
-console.log(`${person.age}`);
+console.log(`${person.name}`);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toLowerCase());
+}
