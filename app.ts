6480bfb38d35ef3a5dedb0b9c910778ee1a2426e
developerwill <info@wilsongomes.me>
2022-10-31 18:56:42 +0100

    Function Return Types & "void"

--- app.ts
+++ app.ts
@@ -1,22 +1,9 @@
-type combinable = number | string;
-type conversionDescriptor = 'as-number' | 'as-string';
-
-function combine(input1: combinable, input2: combinable, resultConversion: conversionDescriptor) {
-    let result: combinable;
-
-    if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function printResult(num: number) {
+    console.log(`Result: ${num}`);
+}
 
-const combinedStrinAges = combine('30', '26', 'as-number');
-console.log(combinedStrinAges);
-
-const combineNames = combine('Wilson ', 'Gomes', 'as-string');
-console.log(combineNames);
+printResult(add(5, 12));
