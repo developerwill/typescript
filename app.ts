8ad4a7f94c58af79f10ebfde9ea89ad84f768323
developerwill <info@wilsongomes.me>
2022-10-31 18:23:24 +0100

    Working with Enums

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-const person: {
+/* const person: {
     name: string;
     age: number;
     hobbies: string[];
@@ -8,12 +8,28 @@ const person: {
     age: 30,
     hobbies: ['Sports', 'Cooking'],
     role: [2, 'author'],
+}; */
+
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR,
+}
+
+const person = {
+    name: 'Wilson',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN,
 };
 
-person.role.push('admin');
-
-console.log(`${person.role}`);
+//person.role.push('admin');
+//console.log(`${person.role}`);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toLowerCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log(`Is admin`);
 }
