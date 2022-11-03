5449b616c0273c288fae61506c607528881beb5f
developerwill <info@wilsongomes.me>
2022-11-03 18:25:21 +0100

    Overriding properties & The "protected" modifier

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
     //private id: string;
     //private name: string;
-    private employees: string[];
+    protected employees: string[];
 
     constructor(private readonly id: number, public name: string) {
         //this.name = name;
@@ -35,6 +35,13 @@ class AccountingDepartment extends Department {
         super(id, 'Accounting');
     }
 
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+
     addReport(text: string) {
         this.reports.push(text);
     }
@@ -61,7 +68,11 @@ const accounting = new AccountingDepartment(1, []);
 accounting.addReport('Something went wrong...');
 accounting.addReport('All good!');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Other than Max');
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // Creates a new object with the class propeties plus methods
 /* const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
