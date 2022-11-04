5423689aa87dc181d62e7ad5e09695c486e1d035
developerwill <info@wilsongomes.me>
2022-11-04 18:33:23 +0100

    Type guard with typeof

---
+++ examples/type-guard-with-typeof.ts
@@ -0,0 +1,11 @@
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
