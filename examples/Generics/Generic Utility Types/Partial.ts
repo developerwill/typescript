1e8315c8b13e871aefbe36a0863cb8af00940b57
developerwill <info@wilsongomes.me>
2022-11-07 13:58:27 +0100

    Generic Partial type

---
+++ examples/Generics/Generic Utility Types/Partial.ts
@@ -0,0 +1,15 @@
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date) {
+    let courseGoal: Partial<CourseGoal> = {};
+
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+
+    return courseGoal as CourseGoal;
+}
